38c55420c10e0f800f4bcf8c56c18ef454ff4cd1
Robert Yokota <rayokota@gmail.com>
2024-07-28 10:10:00 -0700

    Fix leader election

--- src/test/java/io/kcache/kwack/util/ClusterTestHarness.java
+++ src/test/java/io/kcache/kwack/util/ClusterTestHarness.java
@@ -92,6 +92,7 @@ public abstract class ClusterTestHarness {
 
     protected void injectProperties(Properties props) {
         props.setProperty("auto.create.topics.enable", "true");
+        props.setProperty("auto.leader.rebalance.enable", "false");
         props.setProperty("num.partitions", "1");
     }
 
